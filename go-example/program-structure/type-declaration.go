6e99f6eca9bb41b9bf2258d4932c78a9a69876cf
Arda Parlak <35842477+ardpar@users.noreply.github.com>
2022-08-07 21:56:14 +0300

    type declaration new think

--- go-example/program-structure/type-declaration.go
+++ go-example/program-structure/type-declaration.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Celsius float64
 type Fahrenheit float64
 
@@ -13,5 +15,13 @@ func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 52) }
 func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
 
 func main() {
-
+	var c Celsius
+	var f Fahrenheit
+	fmt.Println(c == 0)
+	fmt.Println(f == 0)
+	//fmt.Println(c == f)
+	fmt.Println(c == Celsius(f))
+	fmt.Println(CToF(Boiling))
+	fmt.Println(Fahrenheit(Boiling))
+	fmt.Println(FToC(Fahrenheit(Boiling)))
 }
